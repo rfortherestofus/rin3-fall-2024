34b6c60cd1298b2bdcc78173f6bd7d062998c9bc
David Keyes <david@rfortherestofus.com>
2024-10-31 13:12:04 -0700

    Update function-arguments.R

--- misc/function-arguments.R
+++ misc/function-arguments.R
@@ -6,14 +6,33 @@ gss_cat |>
 # Write code to count the number of unique responses to the partyid question
 
 gss_cat |>
-  separate_longer_delim(cols = partyid,
-                        delim = ",") |> 
+  separate_longer_delim(
+    cols = partyid,
+    delim = ","
+  ) |>
   select(partyid)
 
 gss_cat |>
-  separate_longer_delim(partyid,
-                        delim = ",") |>
-  count(partyid) |>
-  ggplot(aes(partyid,
-             n)) +
-  geom_col()
+  separate_longer_delim(
+    partyid,
+    ","
+  ) |>
+  select(partyid)
+
+ggplot(
+  data = gss_cat,
+  mapping = aes(
+    x = age,
+    y = tvhours
+  )
+) +
+  geom_point()
+
+ggplot(
+  gss_cat,
+  aes(
+    age,
+    tvhours
+  )
+) +
+  geom_point()
