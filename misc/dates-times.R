991166e3451d3007f31ac2e43737c458c596b3a7
David Keyes <david@rfortherestofus.com>
2024-12-05 10:56:25 -0800

    Update dates-times.R

--- misc/dates-times.R
+++ misc/dates-times.R
@@ -5,4 +5,36 @@ library(janitor)
 # https://or.water.usgs.gov/non-usgs/bes/hayden_island.rain
 
 portland_precipitation <-
-  read_csv("data-raw/portland-precipitation.csv") 
+  read_csv("data-raw/portland-precipitation.csv") |> 
+  clean_names() |> 
+  mutate(date = dmy(date)) |> 
+  mutate(total = parse_number(total)) |> 
+  mutate(total = total * 0.01) |> 
+  mutate(
+    year = year(date),
+    month = month(date),
+    day = day(date)
+  ) |> 
+  pivot_longer(
+    cols = starts_with("x"),
+    names_to = "hour",
+    values_to = "precipitation"
+  ) |> 
+  mutate(hour = parse_number(hour)) |> 
+  mutate(precipitation = parse_number(precipitation)) |> 
+  mutate(date_time = make_datetime(
+    year = year,
+    month = month,
+    day = day,
+    hour = hour
+  )) |> 
+  select(date_time, precipitation)
+
+portland_precipitation |> 
+  ggplot(
+    aes(
+      x = date_time,
+      y = precipitation
+    )
+  ) +
+  geom_col()
